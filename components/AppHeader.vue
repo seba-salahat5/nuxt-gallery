dd71b399864c20bd6004ba186fdbc477cc7dd7f1
seba-salahat5 <80277805+seba-salahat5@users.noreply.github.com>
2024-03-23 23:28:32 +0200

    Implementing UI for image upload

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,8 +1,20 @@
 <template>
     <header class="mt-20 bg-white flex justify-between content-baseline sticky ">
         <h1 class="text-xl text-text font-bold my-2">Gallery</h1>
-        <button class="py-2.5 px-6 rounded-full h-11 mb-3 bg-brand text-base text-text font-normal drop-shadow">
+        <button
+        class="py-2.5 px-6 rounded-full h-11 mb-3 bg-brand text-base text-text font-normal drop-shadow"
+        @click="onClickHandler">
             Upload
         </button>
     </header>
 </template>
+
+<script>
+export default {
+    methods: {
+        onClickHandler() {
+            this.$emit("upload-click");
+        }
+    }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="md:mx-28 mx-10">
-        <AppHeader />
+        <AppHeader @upload-click="toggleUpload" />
         <hr class="border-t border-line my-0">
-        <div>
+        <div v-if="!showUpload">
             <slot />
+        </div>
+        <div v-else>
+            <DropzoneComponent @cancel="toggleUpload" />
         </div>
     </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      showUpload: false
+    };
+  },
+  methods: {
+    toggleUpload() {
+      this.showUpload = !this.showUpload;
+    }
+  }
+};
+</script>
