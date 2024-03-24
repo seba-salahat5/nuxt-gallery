d95410ae02caf66ce4525c81f959a47cdc7f5dbb
seba-salahat5 <80277805+seba-salahat5@users.noreply.github.com>
2024-03-25 01:36:56 +0200

    Edit the UI live updating

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,37 @@
 <template>
-  <div class="mt-24 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-cols-1 gap-3">
+  <div v-if="isStoreInitialized" class="mt-24 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-cols-1 gap-3">
     <ImageCard v-for="(image, index) in uploadedImages" :key="index" :src="image" />
+  </div>
+  <div v-else>
+    Loading...
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeMount } from 'vue';
+import { defineComponent, ref, onBeforeMount } from 'vue';
 import { useImageStore } from '~/store/index';
 
 export default defineComponent({
   setup() {
     const imageStore = useImageStore();
-    // Initialize the store before mounting the component
-    onBeforeMount(() => {
+    const uploadedImages = ref<string[]>([]);
+    const isStoreInitialized = ref(false);
+
+    onBeforeMount(async () => {
       console.log("Initializing store...");
-      imageStore.initializeStore();
+      await imageStore.initializeStore();
+      uploadedImages.value = imageStore.getUploadedImages;
+      isStoreInitialized.value = true;
     });
 
     return {
-      uploadedImages: imageStore.getUploadedImages,
+      uploadedImages,
+      isStoreInitialized,
     };
   },
 });
 </script>
+
+
+
+
--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia';
+/*import { defineStore } from 'pinia';
 
 export const useImageStore = defineStore('image', {
     state: () => ({
@@ -11,6 +11,32 @@ export const useImageStore = defineStore('image', {
       },
 
       initializeStore() {
+        const storedImages = localStorage.getItem('uploadedImages');
+        if (storedImages) {
+          this.uploadedImages = JSON.parse(storedImages);
+        }
+      },
+    },
+    getters: {
+      getUploadedImages(): string[] {
+        return this.uploadedImages;
+      },
+    },
+  });*/
+
+  import { defineStore } from 'pinia';
+
+export const useImageStore = defineStore('image', {
+    state: () => ({
+      uploadedImages: [] as string[],
+    }),
+    actions: {
+      addImage(image: string) {
+        this.uploadedImages.push(image);
+        localStorage.setItem('uploadedImages', JSON.stringify(this.uploadedImages));
+      },
+
+      async initializeStore() {
         const storedImages = localStorage.getItem('uploadedImages');
         if (storedImages) {
           this.uploadedImages = JSON.parse(storedImages);
