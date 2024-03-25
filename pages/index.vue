5b545216437139c2df465173b81edb035d831658
seba-salahat5 <80277805+seba-salahat5@users.noreply.github.com>
2024-03-25 05:45:55 +0200

    Editing delete icon in mobile view

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="isStoreInitialized" class="mt-24 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-cols-1 gap-3">
+  <div v-if="isStoreInitialized" class="mt-24 mb-4 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-cols-1 gap-4">
     <ImageCard v-for="(image, index) in uploadedImages" :key="index" :src="image" />
   </div>
   <div v-else>
